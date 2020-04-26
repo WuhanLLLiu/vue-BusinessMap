b5aa31264ef0d7e38d0766bb1ae9a6916f99fe94
WuhanLLLiu <450994973@qq.com>
2020-04-26 11:37:20 +0800

    pic

--- src/views/LYXX.vue
+++ src/views/LYXX.vue
@@ -72,9 +72,9 @@
           <el-row id="rowly_5" class="category">
               <i id = 'icon1' class="el-icon-picture"></i>
               <span id="rowly_42" @click="PICAlert">查看照片</span>
-              <img id = 'img1' src="" class="pic" @click="funcReadImgInfo1"/>
-              <img id = 'img2' src="" class="pic" @click="funcReadImgInfo2"/>
-              <img id = 'img3' src="" class="pic" @click="funcReadImgInfo3"/>
+              <img id = 'img1' src="" class="pic" @click="funcReadImgInfo"/>
+              <img id = 'img2' src="" class="pic" @click="funcReadImgInfo"/>
+              <img id = 'img3' src="" class="pic" @click="funcReadImgInfo"/>
           </el-row>
         </div>
     </div>
@@ -169,60 +169,9 @@
         })
       },
       // 点击查看大图
-      funcReadImgInfo1(){
-        let element = document.getElementById('img1');
-          if (this.fullscreen) {
-            if (document.exitFullscreen) {
-              document.exitFullscreen();
-            } else if (document.webkitCancelFullScreen) {
-              document.webkitCancelFullScreen();
-            } else if (document.mozCancelFullScreen) {
-              document.mozCancelFullScreen();
-            } else if (document.msExitFullscreen) {
-              document.msExitFullscreen();
-            }
-          } else {
-            if (element.requestFullscreen) {
-              element.requestFullscreen();
-            } else if (element.webkitRequestFullScreen) {
-              element.webkitRequestFullScreen();
-            } else if (element.mozRequestFullScreen) {
-              element.mozRequestFullScreen();
-            } else if (element.msRequestFullscreen) {
-              // IE11
-              element.msRequestFullscreen();
-            }
-          }
-          this.fullscreen = !this.fullscreen;
-      },
-      funcReadImgInfo2(){
-        let element = document.getElementById('img2');
-          if (this.fullscreen) {
-            if (document.exitFullscreen) {
-              document.exitFullscreen();
-            } else if (document.webkitCancelFullScreen) {
-              document.webkitCancelFullScreen();
-            } else if (document.mozCancelFullScreen) {
-              document.mozCancelFullScreen();
-            } else if (document.msExitFullscreen) {
-              document.msExitFullscreen();
-            }
-          } else {
-            if (element.requestFullscreen) {
-              element.requestFullscreen();
-            } else if (element.webkitRequestFullScreen) {
-              element.webkitRequestFullScreen();
-            } else if (element.mozRequestFullScreen) {
-              element.mozRequestFullScreen();
-            } else if (element.msRequestFullscreen) {
-              // IE11
-              element.msRequestFullscreen();
-            }
-          }
-          this.fullscreen = !this.fullscreen;
-      },
-      funcReadImgInfo3(){
-        let element = document.getElementById('img3');
+      funcReadImgInfo:function(event){
+        console.log(event)
+        let element = document.getElementById(event.target.id);
           if (this.fullscreen) {
             if (document.exitFullscreen) {
               document.exitFullscreen();
