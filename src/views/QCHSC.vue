bd64e2f0367cef9fbd80257b608ed6bb8aaf3917
pei28 <15071274877@163.com>
2020-05-17 21:37:34 +0800

    commit

--- src/views/QCHSC.vue
+++ src/views/QCHSC.vue
@@ -30,6 +30,76 @@
               <img src="http://121.196.60.135/cdn/汽车后市场/汉阳区汽车后市场情况.jpg" id='hj'/>
           </div>
       </van-tab>
+      <van-tab title="武汉汽车运动文化特色小镇创建规划">
+          <div id='QCHSC0'>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00001.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00002.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00003.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00004.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00005.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00006.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00007.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00008.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00009.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00010.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00011.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00012.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00013.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00014.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00015.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00016.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00017.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00018.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00019.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00020.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00021.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00022.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00023.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00024.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00025.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00026.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00027.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00028.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00029.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00030.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00031.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00032.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00033.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00034.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00035.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00036.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00037.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00038.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00039.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00040.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00041.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00042.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00043.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00044.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00045.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00046.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00047.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00048.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00049.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00050.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00051.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00052.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00053.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00054.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00055.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00056.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00057.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00058.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00059.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00060.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00061.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00062.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00063.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00064.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00065.jpeg" id='hj'/>
+              <img src="http://121.196.60.135/cdn/武汉汽车运动文化特色小镇创建规划/IMG_00066.jpeg" id='hj'/>
+          </div>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
--- src/views/ZSTJ2.vue
+++ src/views/ZSTJ2.vue
@@ -1,51 +1,23 @@
 <template>
   <div id="ZSTJ2" class="layout">
     <div id="row">
-      <p id="row1">武汉汉阳招商资源推介</p>
+      <p id="row1">武汉汉阳招商资源指南</p>
       <!-- <p id="row2">来源：汉阳区商务局</p> -->
     </div>
 
     <el-divider class="el-divider1"></el-divider>
 
     <div id="Selfform">
-      <img src="./../assets/zstj/1.jpg" id="hj" />
-      <img src="./../assets/zstj/2.jpg" id="hj" />
-      <img src="./../assets/zstj/3.jpg" id="hj" />
-      <img src="./../assets/zstj/4.jpg" id="hj" />
-      <img src="./../assets/zstj/5.jpg" id="hj" />
-      <img src="./../assets/zstj/6.jpg" id="hj" />
-      <img src="./../assets/zstj/7.jpg" id="hj" />
-      <img src="./../assets/zstj/8.jpg" id="hj" />
-      <img src="./../assets/zstj/9.jpg" id="hj" />
-      <img src="./../assets/zstj/10.jpg" id="hj" />
-      <img src="./../assets/zstj/11.jpg" id="hj" />
-      <img src="./../assets/zstj/12.jpg" id="hj" />
-      <img src="./../assets/zstj/13.jpg" id="hj" />
-      <img src="./../assets/zstj/14.jpg" id="hj" />
-      <img src="./../assets/zstj/15.jpg" id="hj" />
-      <img src="./../assets/zstj/16.jpg" id="hj" />
-      <img src="./../assets/zstj/17.jpg" id="hj" />
-      <img src="./../assets/zstj/18.jpg" id="hj" />
-      <img src="./../assets/zstj/19.jpg" id="hj" />
-      <img src="./../assets/zstj/20.jpg" id="hj" />
-      <img src="./../assets/zstj/21.jpg" id="hj" />
-      <img src="./../assets/zstj/22.jpg" id="hj" />
-      <img src="./../assets/zstj/23.jpg" id="hj" />
-      <img src="./../assets/zstj/24.jpg" id="hj" />
-      <img src="./../assets/zstj/25.jpg" id="hj" />
-      <img src="./../assets/zstj/26.jpg" id="hj" />
-      <img src="./../assets/zstj/27.jpg" id="hj" />
-      <img src="./../assets/zstj/28.jpg" id="hj" />
-      <img src="./../assets/zstj/29.jpg" id="hj" />
-      <img src="./../assets/zstj/30.jpg" id="hj" />
-      <img src="./../assets/zstj/31.jpg" id="hj" />
-      <img src="./../assets/zstj/32.jpg" id="hj" />
-      <img src="./../assets/zstj/33.jpg" id="hj" />
-      <img src="./../assets/zstj/34.jpg" id="hj" />
-      <img src="./../assets/zstj/35.jpg" id="hj" />
-      <img src="./../assets/zstj/36.jpg" id="hj" />
-      <img src="./../assets/zstj/37.jpg" id="hj" />
-      <img src="./../assets/zstj/38.jpg" id="hj" />
+      <img src="./../assets/zstj/IMG_00001.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00002.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00003.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00004.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00005.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00006.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00007.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00008.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00009.jpeg" id="hj" />
+	  <img src="./../assets/zstj/IMG_00010.jpeg" id="hj" />
     </div>
   </div>
 </template>
