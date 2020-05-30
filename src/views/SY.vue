02f8e94d1bc2b2623f1d36e231f9f2d778b7b40c
axmand <815966508@qq.com>
2020-05-30 09:33:08 +0800

    revise

--- src/views/SY.vue
+++ src/views/SY.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="SY" class="layout">
+
     <div class="logo">
       <img id="logo" src="./../assets/logo.png" width="40%" />
     </div>
@@ -24,7 +25,12 @@
 </template>
 
 <script>
+
 import Vue from "vue";
+
+// 微信分享拦截，转用SDK指定分享信息
+// import wx from 'weixin-js-sdk';
+
 export default {
   name: "SY",
 
@@ -76,8 +82,29 @@ export default {
   },
 
   mounted() {
-    //增加统计图表
+//     //增加统计图表
+//     //增加微信分享页面信息
+//     wx.config({    
+// 　　  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。　　
+// 　　  appId: '', // 必填，公众号的唯一标    
+// 　　  timestamp: , // 必填，生成签名的时间戳
+// 　　  nonceStr: '', // 必填，生成签名的随机串
+// 　　  signature: '',// 必填，签名
+// 　　  jsApiList: [] // 必填，需要使用的JS接口列表
+//     });
+//     wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+// 		    wx.updateAppMessageShareData({ 
+// 		　　　　title: '', // 分享标题
+// 		　　　　desc: '', // 分享描述
+// 		　　　　link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+// 		　　　　imgUrl: '', // 分享图标
+// 		       success: function () {          
+//               // 设置成功
+//               }
+//       });
+//     });
   }
+
 };
 </script>
 
