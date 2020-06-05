b45ab67be9e1fc2b80b95280a0786df07e847cb6
axmand <815966508@qq.com>
2020-06-05 19:46:40 +0800

    fixed SY.vue

--- src/views/SY.vue
+++ src/views/SY.vue
@@ -1,392 +1,187 @@
 <template>
-  <div id="SY" class="layout">
-
-    <div class="logo">
-      <img id="logo" src="./../assets/logo.png" width="40%" />
-    </div>
-
-    <img src="./../assets/新手引导.png" width="8%" id='icon1' @click="RouteFWZY"/>
-    <img src="./../assets/欢迎页.png" width="8%" id="icon2" @click="RouteHYC"/>
-
-    <div id="buildings">
-      <img id="building" src="./../assets/土地信息.png" width="30%" @click="RouteDT" />
-      <img id="building" src="./../assets/楼宇信息.png" width="30%" @click="RouteLY" />
-    </div>
-    <div id="buildings0">
-      <img id="building" src="./../assets/招商政策.png" width="30%" @click="RouteZSZC" />
-      <img id="building" src="./../assets/招商推介.png" width="30%" @click="RouteZSTJ" />
-      <img id="building" src="./../assets/重点产业.png" width="30%" @click="RouteZDCY" />
-    </div>
-
-    <div class="bottom-footer">
-      版权所有©汉阳区商务局&nbsp;&nbsp;&nbsp;技术支持:武汉大学测绘学院
-    </div>
-  </div>
+    <el-container direction='vertical' class="bg">
+        <el-header type="flex" justify="end">
+            <el-col :span="3">
+                <el-image style="width:30px; height:34px" :src="images[5]" fit="fill" @click="RouteFWZY"></el-image>
+            </el-col>
+            <el-col :span="3">
+                <el-image style="width:30px; height:34px" :src="images[6]" fit="fill" @click="RouteHYC"></el-image>
+            </el-col>
+        </el-header>
+        <el-main>
+            <el-row type="flex" justify="center" class="logo">
+                <el-col :span="28">
+                    <el-image style="width: 100%; height: 100%" :src="logo" fit="fill"></el-image>
+                </el-col>
+            </el-row>
+            <el-row type="flex" justify="center">
+                <el-col :span="8">
+                    <el-image
+                        style="width: 100px; height: 110px"
+                        :src="images[0]"
+                        fit="fill"
+                        @click="RouteDT"></el-image>
+                </el-col>
+                <el-col :span="8">
+                    <el-image
+                        style="width: 100px; height: 110px"
+                        :src="images[1]"
+                        fit="fill"
+                        @click="RouteLY"></el-image>
+                </el-col>
+            </el-row>
+            <el-row type="flex" justify="center">
+                <el-col :span="8" style="al">
+                    <el-image
+                        style="width: 100px; height: 110px"
+                        :src="images[2]"
+                        fit="fill"
+                        @click="RouteZSZC"></el-image>
+                </el-col>
+                <el-col :span="8">
+                    <el-image
+                        style="width: 100px; height: 110px"
+                        :src="images[3]"
+                        fit="fill"
+                        @click="RouteZSTJ"></el-image>
+                </el-col>
+                <el-col :span="8">
+                    <el-image
+                        style="width: 100px; height: 110px"
+                        :src="images[4]"
+                        fit="fill"
+                        @click="RouteZDCY"></el-image>
+                </el-col>
+            </el-row>
+        </el-main>
+        <el-footer class="text">
+            {{footerText}}
+        </el-footer>
+    </el-container>
 </template>
 
 <script>
 
-import Vue from "vue";
+    import Vue from "vue";
 
-// 微信分享拦截，转用SDK指定分享信息
-// import wx from 'weixin-js-sdk';
+    // 微信分享拦截，转用SDK指定分享信息 import wx from 'weixin-js-sdk';
 
-export default {
-  name: "SY",
+    export default {
+        name: "SY",
 
-  components: {},
+        components: {},
 
-  data() {
-    return {
-      isFull: false,
-      totalData: {
-        cure: 16,
-        confirm: 17,
-        new: 0
-      },
-      outerVisible: false,
-      innerVisible_0: false,
-      innerVisible_1: false,
-      modalvisible: false,
-      alertText: "",
-      fullscreen1: true
+        data() {
+            return {
+                logo: 'http://www.hyzsydt.top/cdn/ui/logo.png',
+                images: [
+                    'http://www.hyzsydt.top/cdn/ui/土地信息.png', 
+                    'http://www.hyzsydt.top/cdn/ui/楼宇信息.png', 
+                    'http://www.hyzsydt.top/cdn/ui/招商政策.png', 
+                    'http://www.hyzsydt.top/cdn/ui/招商推介.png', 
+                    'http://www.hyzsydt.top/cdn/ui/重点产业.png',
+                    'http://www.hyzsydt.top/cdn/ui/新手引导.png',
+                    'http://www.hyzsydt.top/cdn/ui/欢迎页.png'
+                ],
+                isFull: false,
+                totalData: {
+                    cure: 16,
+                    confirm: 17,
+                    new: 0
+                },
+                footerText: '版权所有©汉阳区商务局   技术支持:武汉大学测绘学院',
+                outerVisible: false,
+                innerVisible_0: false,
+                innerVisible_1: false,
+                modalvisible: false,
+                alertText: "",
+                fullscreen1: true
+            };
+        },
+
+        methods: {
+            RouteZSTJ() {
+                this
+                    .$router
+                    .push({path: "/ZSTJ2"});
+            },
+            RouteDT() {
+                this
+                    .$router
+                    .push({path: "/TDXX"});
+            },
+            RouteLY() {
+                this
+                    .$router
+                    .push({path: "/LYXX"});
+            },
+            RouteZSZC() {
+                this
+                    .$router
+                    .push({path: "/ZSZC"});
+            },
+            RouteZDCY() {
+                this
+                    .$router
+                    .push({path: "/ZDCY"});
+            },
+            RouteHYC() {
+                this
+                    .$router
+                    .push({path: "/HYC"});
+            },
+            RouteFWZY() {
+                this
+                    .$router
+                    .push({path: "/FWZY"});
+            }
+        },
+
+        mounted() {}
+
     };
-  },
-
-  beforeMount() {
-    this.carouselHeight = window.innerHeight + "px";
-  },
-
-  methods: {
-    RouteZSTJ() {
-      this.$router.push({ path: "/ZSTJ2" });
-    },
-    RouteDT() {
-      this.$router.push({ path: "/TDXX" });
-    },
-    RouteLY() {
-      this.$router.push({ path: "/LYXX" });
-    },
-    RouteZSZC() {
-      this.$router.push({ path: "/ZSZC" });
-    },
-    RouteZDCY() {
-      this.$router.push({ path: "/ZDCY" });
-    },
-    RouteHYC() {
-      this.$router.push({ path: "/HYC" });
-    },
-    RouteFWZY() {
-      this.$router.push({ path: "/FWZY" });
-    },
-  },
-
-  mounted() {
-//     //增加统计图表
-//     //增加微信分享页面信息
-//     wx.config({    
-// 　　  debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。　　
-// 　　  appId: '', // 必填，公众号的唯一标    
-// 　　  timestamp: , // 必填，生成签名的时间戳
-// 　　  nonceStr: '', // 必填，生成签名的随机串
-// 　　  signature: '',// 必填，签名
-// 　　  jsApiList: [] // 必填，需要使用的JS接口列表
-//     });
-//     wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
-// 		    wx.updateAppMessageShareData({ 
-// 		　　　　title: '', // 分享标题
-// 		　　　　desc: '', // 分享描述
-// 		　　　　link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-// 		　　　　imgUrl: '', // 分享图标
-// 		       success: function () {          
-//               // 设置成功
-//               }
-//       });
-//     });
-  }
-
-};
 </script>
 
-<style scoped>
-* {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
+<style scoped="scoped">
 
-body {
-  overflow: hidden;
-}
+    body {
+        overflow: hidden;
+    }
 
-.layout {
-  background: url(./../assets/汉阳-00.png) no-repeat center 0;
-  background-size: 100% 100%;
-  -moz-background-size: 100% 100%;
-  width: 100%;
-  height: 100%;
-  position: relative;
-  overflow: hidden;
-}
+    .el-header{
+      float: right;
+      padding: 2vw;
+      display: flex;
+      justify-content: end;
+      text-align: center;
+      flex-direction: row-reverse;
+    }
 
-.layout .head {
-  background-size: 50% 50%;
-  height: 20%;
-  width: 100%;
-  font-size: 50px;
-  font: bolder;
-  padding-left: 3%;
-  line-height: 98px;
-  color: #000080;
-}
+    .el-main {
+        padding: 0;
+    }
 
-.layout .logo {
-  /*position: absolute;*/
-  top: 20%;
-  /* border-radius: 50% 50%; */
-}
+    .el-main .el-col {
+        display: flex;
+        justify-content: center;
+    }
 
-#logo {
-  width: 100%;
-  height: auto;
-  max-width: 100%;
-  max-height: 100%;
-  /* border-radius: 50% 50%; */
-  position: absolute !important;
-  left: 50%;
-  top: 25%;
-  transform: translateY(-50%, -50%);
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  -o-transform: translate(-50%, -50%);
-  display: block;
-}
+    .text {
+        display: flex;
+        justify-content: center;
+        text-align: center;
+        font-size: 3.6vw;
+        color: white;
+        text-shadow: 5px 2px 10px #000;
+        /* 垂直居中 */
+        /* align-items: center;  */
+    }
 
-.layout .select {
-  position: absolute;
-  z-index: 102;
-  height: 36px;
-  background: rgba(255, 255, 255, 1);
-  box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.08),
-    0px 0px 1px 0px rgba(0, 0, 0, 0.08);
-  border-radius: 6px;
-  margin: 0.3rem 0.3rem 0px 0.3rem;
-  width: 20%;
-}
-
-.bottom.full {
-  transform: translateY(-897px);
-}
-
-.bottom-btn-title {
-  float: left;
-}
-
-.fengxian {
-  float: left;
-  margin-left: 20px;
-  font-size: 20px;
-  padding: 0 20px;
-  height: 34px;
-  line-height: 34px;
-  background: #97e290;
-  border-radius: 14.5px;
-  margin-top: 23px;
-}
-
-.bottom-center {
-  height: 182px;
-  transition: all 0.5s;
-  width: 100%;
-  padding-top: 1px;
-  border-bottom: 2px solid #ccc;
-  position: relative;
-}
-
-.number-list {
-  height: 78px;
-  margin-top: 33px;
-  position: relative;
-}
-
-.number-list > li.number-list-item {
-  height: 100%;
-  width: 118px;
-  font-size: 28px;
-  position: absolute;
-  left: 88px;
-  top: 0;
-  text-align: center;
-}
-
-.number-list > li.number-list-item:nth-child(2) {
-  left: 321px;
-}
-
-.number-list > li.number-list-item:nth-child(3) {
-  right: 80px;
-  left: auto;
-}
-
-.list-item-top.green {
-  color: #0f9854;
-}
-
-.list-item-top {
-  height: 28px;
-  margin-bottom: 14px;
-  font-size: 30px;
-  text-align: center;
-  color: #ff5357;
-}
-
-.list-item-bottom {
-  height: 26px;
-}
-
-.bottom-center-info {
-  height: 34px;
-  width: 659px;
-  position: absolute;
-  bottom: 21px;
-  left: 50%;
-  transform: translateX(-50%);
-  background: pink;
-  border-radius: 17px;
-}
-
-.bottom-footer {
-  height: 71px;
-  text-align: center;
-  line-height: 71px;
-  color: white;
-  font-size: 0.8em;
-  margin-top: 1.5em;
-  margin-bottom: 12px;
-  width: 100%;
-  position: absolute;
-  bottom: 35px;
-  transition: all 0.5s;
-  z-index: 4;
-  opacity: 100%;
-  text-shadow:5px 2px 10px #000
-}
-
-.map {
-  /* height: calc(100% - 431px); */
-  height: calc(100%);
-  overflow: hidden;
-  position: relative;
-}
-
-.center {
-  height: 178px;
-}
-
-.center .definite {
-  height: 60px;
-  margin-top: 16px;
-  font-size: 30px;
-  font-weight: 900;
-  margin-left: 44px;
-  color: #393a35;
-  letter-spacing: 2px;
-}
-
-.people {
-  height: 60px;
-  font-size: 30px;
-  color: #e75f5c;
-  margin-left: 5px;
-}
-
-.alert {
-  width: 540px;
-  border: 2px solid #112daf;
-  border-radius: 5px;
-  margin-left: 44px;
-  color: #3a3b36;
-  text-align: center;
-  line-height: 48px;
-  font-size: 20px;
-  letter-spacing: 5px;
-  padding-left: 4px;
-  font-weight: bold;
-  margin-top: 5px;
-}
-
-.monad {
-  font-size: 16px;
-}
-
-.ul {
-  display: table;
-  list-style-type: none;
-  text-align: center;
-}
-
-#row1 {
-  padding-top: 1.5em;
-  padding-left: 1em;
-  text-align: center;
-  /* box-shadow: 0 0 5vmax 50vmax rgba(0,0,0,.5); */
-}
-
-.el-col {
-  border-radius: 4px;
-  width: 23%;
-  text-align: center;
-}
-
-#img {
-  text-align: center;
-}
-
-#buildings {
-  text-align: center;
-  position: absolute;
-  bottom: 35%;
-}
-#buildings0 {
-  text-align: center;
-  position: absolute;
-  bottom: 20%;
-}
-#building {
-  padding: 1.5%;
-}
-#icon1{
-  background-color:transparent;
-  color: white;
-  padding-right: 2%;
-  /* font-size: 2em; */
-  /* border-radius: 50% 50%; */
-  position: absolute !important;
-  right: 10%;
-  top: 1%;
-  /* transform: translateY(-50%, -50%);
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  -o-transform: translate(-50%, -50%); */
-  display: block;
-}
-#icon2{
-  background-color:transparent;
-  color: white;
-  padding-right: 2%;
-  /* font-size: 2em; */
-  right: 0%;
-  /* left: 3%; */
-  /* border-radius: 50% 50%; */
-  position: absolute !important;
-  top: 1%;
-  /* transform: translateY(-50%, -50%);
-  -webkit-transform: translate(-50%, -50%);
-  -moz-transform: translate(-50%, -50%);
-  -ms-transform: translate(-50%, -50%);
-  -o-transform: translate(-50%, -50%); */
-  display: block;
-}
+    .bg {
+        display: flex;
+        height: 100%;
+        width: 100%;
+        background-size: 100% 100%;
+        background-image: url("./../assets/汉阳-00.png");
+    }
 </style>
